43dd0390b97a633f981a42cd3fb9533da54f39ba
Harika-Raj <r121662@rguktrkv.ac.in>
2019-01-28 16:51:05 +0530

    Store images in firebase

--- Components/ProfilePage.js
+++ Components/ProfilePage.js
@@ -1,39 +1,79 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Image, TouchableOpacity,Button } from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity,Platform} from 'react-native';
 import ImagePicker from "react-native-image-picker";
+import firebase from "../firebase/firebase";
+import RNFetchBlob from 'react-native-fetch-blob'
 
+
+const Blob = RNFetchBlob.polyfill.Blob
+const fs = RNFetchBlob.fs
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+window.Blob = Blob
+var options = {
+    title : "Pick an Image",
+    maxWidth: 800,
+    maxHeight: 600,
+    storageOptions: {
+        path: 'sourceImages',
+    },
+};
 export default class Profile extends Component {
-    state = {
-        pickedImage: null
+    constructor(props){
+        super();
+        this.state = {
+            image_uri: null
+        }
     }
-    pickImageHandler = () => {
-        const options = {
-            title : "Pick an Image",
-            maxWidth: 800,
-            maxHeight: 600,
-            storageOptions: {
-                path: 'sourceImages',
-            },
+        uploadImage(uri, mime = 'image/jpeg')
+        {
+            return new Promise((resolve, reject) => {
+                let imageURI = uri;
+                let uploadBlob = null;
+                const uploadUri = Platform.OS === 'ios' ? imageURI.replace('file://', '') : imageURI
+                const imageRef = firebase.storage().ref('images').child('image_003')
+                console.log("   jbjb :" + imageRef);
+                fs.readFile(uploadUri, 'base64')
+                    .then((data) => {
+                        return Blob.build(data, {type: `${mime};BASE64`})
+                    })
+                    .then((blob) => {
+                        uploadBlob = blob
+                        return imageRef.put(blob, {contentType: mime})
+                    })
+                    .then(() => {
+                        uploadBlob.close()
+                        return imageRef.getDownloadURL()
+                    })
+                    .then((url) => {
+                        resolve(url)
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+            })
 
-        };
-
-
-        ImagePicker.showImagePicker(options, (responce) => {
-            if (responce.didCancel) {
-                console.log("User cancelled!");
-            } else if (responce.error) {
-                console.log("Error", responce.error);
-            } else {
-                const source = {
-                    uri: responce.uri,
+        }
+        pickImageHandler()
+        {
+            ImagePicker.showImagePicker(options, (responce) => {
+                console.log('Response = ', +responce);
+                if (responce.didCancel) {
+                    console.log("User cancelled!");
+                } else if (responce.error) {
+                    console.log("Error", responce.error);
+                } else {
+                    let image_uri = [];
+                    this.uploadImage(responce.uri)
+                        .then(url => {
+                            this.setState({
+                                image_uri: responce.uri
+                            })
+                        })
+                        .catch(error => console.log(error))
                 }
-                this.setState({
-                    pickedImage: source,
-                });
 
-            }
-        });
-    }
+            });
+        }
     render() {
         return (
             <View style={styles.container}>
@@ -44,10 +84,10 @@ export default class Profile extends Component {
                 </View>
                 <View>
                     <View style={styles.imageContainer}>
-                        <TouchableOpacity  onPress={this.pickImageHandler}>
-                            {  this.state.pickedImage === null ?
+                        <TouchableOpacity  onPress={this.pickImageHandler.bind(this)}>
+                            {   this.state.image_uri === null ?
                                 <Image style={styles.placeholder} source={require('../Icon/userIcon1.png')}/>:
-                                <Image style={styles.placeholder} source={this.state.pickedImage}  />
+                                <Image style={styles.placeholder} source={{uri : this.state.image_uri}}/>
                             }
                         </TouchableOpacity>
                     </View>
--- android/app/src/main/java/com/chatbox/MainApplication.java
+++ android/app/src/main/java/com/chatbox/MainApplication.java
@@ -3,12 +3,14 @@ package com.chatbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.imagepicker.ImagePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,8 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new ReactNativeContacts(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new ImagePickerPackage()
       );
     }
 
